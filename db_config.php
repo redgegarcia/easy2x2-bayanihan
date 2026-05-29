757345489ef2d3c879b037966bc7d56e2349f96d
redgegarcia <69279963+redgegarcia@users.noreply.github.com>
2026-05-29 18:19:31 +0800

    Update db_config.php

--- db_config.php
+++ db_config.php
@@ -1,11 +1,1 @@
-<?php
-$db_host = 'localhost';
-$db_name = 'kayodpin_easy_2x2';
-$db_user = 'kayodpin_admin';
-$db_pass = 'edgie######';
 
-$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-?>
